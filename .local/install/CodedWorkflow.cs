f21ee5af15a3bb733a68dcd66f584d9cf1eb5ddf
JorgeLoaizaChazey <126007785+JorgeLoaizaChazey@users.noreply.github.com>
2023-07-14 16:57:23 +0000

    Avance 100%

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace ALICORP_ExtractosBancariosDispatcher01
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
